be837623051d21c945a61d5f6bd4210516047378
Abhinav Kanogia <abhinavisadev@gmail.com>
2024-12-27 01:01:55 +0530

    new port for cloud

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = 8080;
 
 function convertStringToDate(dateTimeString) {
     var [datePart, timePart] = dateTimeString.split(' '); // Split the date and time parts
